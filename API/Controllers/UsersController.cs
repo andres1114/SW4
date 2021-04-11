d5501e428c94b17080f0d4febda4d99ec7fcec21
Andres Zapata <polako_1114@hotmail.com>
2021-04-11 17:17:37 -0500

    Commit inicial de SW4

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context) {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() {
+            var users = this.context.Users.ToList();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id) {
+            var user = this.context.Users.Find(id);
+            return user;
+        }
+    }
+}
