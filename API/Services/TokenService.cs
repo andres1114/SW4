9fc22eec4e29ebad23f4252fa025fa85d01aabe2
Andres Zapata <polako_1114@hotmail.com>
2021-05-24 08:09:56 -0500

    Implements Jwt library and Token Service / Interface

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using API.Entities;
+using API.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Services {
+    public class TokenService : ITokenService {
+        private readonly SymmetricSecurityKey key;
+        public TokenService(IConfiguration config) {
+            this.key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["tokenKey"]));
+        }
+        public string CreateToken(AppUser user) { 
+            var claims = new List<Claim> { 
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var credentials = new SigningCredentials(this.key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 using API.Data;
+using API.Interface;
+using API.Services;
 
 namespace API
 {
@@ -29,6 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
